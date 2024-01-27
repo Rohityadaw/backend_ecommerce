fb9646ec20bd2c0e1c2346a3a208d72475cd5f70
2018kucp1078@iiitkota.ac.in <2018kucp1078@iiitkota.ac.in>
2024-01-27 16:37:29 +0530

    category

---
+++ src/main/java/com/example/ecommerce/model/Category.java
@@ -0,0 +1,28 @@
+package com.example.ecommerce.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "category")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "category_name")
+    private @NotBlank String categoryName;
+
+    private @NotBlank String description;
+
+    @Column(name = "image_url")
+    private @NotBlank String imageUrl;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
