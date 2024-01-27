a1e558cd5647c61e896b358013ba928029a91427
2018kucp1078@iiitkota.ac.in <2018kucp1078@iiitkota.ac.in>
2024-01-27 14:40:33 +0530

    First commit

---
+++ src/main/java/com/example/ecommerce/NoticeController.java
@@ -0,0 +1,15 @@
+package com.example.ecommerce;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class NoticeController {
+    @GetMapping("/all")
+    String getAll() {
+        return "Rohit";
+    }
+//    List<String> addNotice()
+}
